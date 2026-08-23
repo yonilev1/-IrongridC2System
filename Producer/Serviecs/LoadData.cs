27d49f76dd938db7a0ad609964d7c31e353a4ae6
Yoni <yonilev2021@gmail.com>
2026-08-23 11:20:53 +0300

    Data Loader from Json file - Deserilized to LiveAssets

---
+++ Producer/Serviecs/LoadData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Producer.Models;
+
+namespace Producer.Serviecs;
+
+public class LoadData
+{
+    public List<LiveAssets>? Load(string path)
+    {
+        try
+        {
+            var content = File.ReadAllText(path);
+            List<LiveAssets>? assets = JsonSerializer.Deserialize<List<LiveAssets>>(content);
+            return assets;
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine("File Not Found");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error while reading file: {ex.Message}");
+        }
+        return null;
+    }
+}
