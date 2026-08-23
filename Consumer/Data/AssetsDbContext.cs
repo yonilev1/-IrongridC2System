37c97fead1080282bb815658b3502dfdbb435c75
Yoni <yonilev2021@gmail.com>
2026-08-23 12:02:02 +0300

    added Db Context

---
+++ Consumer/Data/AssetsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer.Data;
+
+public class AssetsDbContext : DbContext
+{
+    public AssetsDbContext(DbContextOptions<AssetsDbContext> options)
+        : base(options) 
+    { }
+}
