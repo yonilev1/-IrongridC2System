24e7a5c12b457bd4584b9b64aac6754ad559c24e
Yoni <yonilev2021@gmail.com>
2026-08-23 11:19:31 +0300

    Live Assets Model

---
+++ Producer/Models/LiveAssets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producer.Models;
+
+public class LiveAssets
+{
+    public int Id { get; set; }
+    public string AssetType { get; set; } = string.Empty;
+    public string RawValue { get; set; } = string.Empty;
+    public DateTime TimeStamp { get; set; }
+}
